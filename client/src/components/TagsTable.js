d9807b922a58957da310ca6c3f71ad3a10e1177a
TalonGrayson <talongrayson@gmail.com>
2020-05-15 07:27:09 +0100

    Specify endpoint port

--- client/src/components/TagsTable.js
+++ client/src/components/TagsTable.js
@@ -10,7 +10,7 @@ dotenv.config();
 const ENDPOINT =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000"
-    : "https://streamiibo.herokuapp.com/";
+    : "https://streamiibo.herokuapp.com:8000/";
 
 const socket = socketIOClient(ENDPOINT);
 
